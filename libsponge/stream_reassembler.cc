47d868b4bc7c48edff6bdfdd59ca7ba08c29115d
Yang6149 <614989768@qq.com>
2020-05-31 23:30:38 +0800

    pass lab1

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -12,15 +12,92 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity)
+    : _output(capacity), _Unassembled(), _firstUnassembled(0), _nUnassembled(0), _capacity(capacity), _eof(0) {
+    std::cout<<"data"<<"\t"<<"index"<<"\n";
+}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
-void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+
+void StreamReassembler::push_substring(const std::string &data, const size_t index, const bool eof) {
+    std::cout<<data<<"\t"<<index<<"\n";
+    _eof = _eof | eof;
+    if (data.size() > _capacity) {
+        _eof = 0;
+    }
+    if (data.empty() || index + data.size() <= _firstUnassembled) {
+        if (_eof)
+            _output.end_input();
+        return;
+    }
+    //这一次最大能存到的index
+    size_t firstUnacceptable = _firstUnassembled + (_capacity - _output.buffer_size());
+
+    //不能直接影响_output的情形，储存的数据提前优化处理
+    std::set<typeUnassembled>::iterator iter2;
+    size_t resIndex = index;
+    auto resData = std::string(data);
+    //把resData 接收过的部分cut掉
+    if (resIndex < _firstUnassembled) {
+        resData = resData.substr(_firstUnassembled - resIndex);
+        resIndex = _firstUnassembled;
+    }
+    //如果这一次的数据大于最大能存到的数据，也截掉一部分
+
+    //检查set当中是否有刚好可以连接在一起的segment，有的话连接在一起
+    for (iter2 = _Unassembled.begin(); iter2 != _Unassembled.end();) {
+        if (size_t deleteNum = merge_substring(resIndex, resData, iter2)) {  //返回值是删掉重合的bytes数
+            _Unassembled.erase(iter2++);
+            _nUnassembled -= deleteNum;
+        } else
+            iter2++;
+    }
+    //如果当前Data里面的数据比capacity-size要大的话，就进行消减然后Insert到Set中
+    if (resIndex + resData.size() > firstUnacceptable){
+        _Unassembled.insert(typeUnassembled(firstUnacceptable, resData.substr(firstUnacceptable - resIndex)));
+        _nUnassembled += resData.substr(firstUnacceptable - resIndex).size();
+        resData = resData.substr(0, firstUnacceptable - resIndex);
+    }
+    //再次判断如果当前index小于第一个没重组的index，就输出
+    if (resIndex <= _firstUnassembled) {
+        size_t wSize = _output.write(string(resData.begin() + _firstUnassembled - resIndex, resData.end()));
+        if (wSize == resData.size() && eof)
+            _output.end_input();
+        _firstUnassembled += wSize;
+    } else {
+
+        //大于的话，就放入set当中，等待被其他的连接再输出
+        _Unassembled.insert(typeUnassembled(resIndex, resData));
+        _nUnassembled += resData.size();
+    }
+    //  当该没有非重组的数据并且eof关闭时，就把数据流也关闭了。
+    if (empty() && _eof) {
+        _output.end_input();
+    }
+    return;
+}
+int StreamReassembler::merge_substring(size_t &index, std::string &data, std::set<typeUnassembled>::iterator iter2) {
+    // return value: 1:successfully merge; 0:fail to merge
+    std::string data2 = (*iter2).data;
+    size_t l2 = (*iter2).index, r2 = l2 + data2.size() - 1;
+    size_t l1 = index, r1 = l1 + data.size() - 1;
+    if (l2 > r1 + 1 || l1 > r2 + 1)
+        return 0;
+    index = min(l1, l2);
+    size_t deleteNum = data2.size();
+    if (l1 <= l2) {
+        if (r2 > r1)
+            data += std::string(data2.begin() + r1 - l2 + 1, data2.end());
+    } else {
+        if (r1 > r2)
+            data2 += std::string(data.begin() + r2 - l1 + 1, data.end());
+        data.assign(data2);
+    }
+    return deleteNum;
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const { return _nUnassembled; }
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const { return _nUnassembled == 0; }
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -3,17 +3,31 @@
 
 #include "byte_stream.hh"
 
+#include <algorithm>
 #include <cstdint>
+#include <iostream>
+#include <map>
+#include <set>
 #include <string>
+class typeUnassembled {
+  public:
+    size_t index;
+    std::string data;
+    typeUnassembled(size_t _index, std::string _data) : index(_index), data(_data) {}
+    bool operator<(const typeUnassembled &t1) const { return index < t1.index; }
+};
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
 class StreamReassembler {
   private:
     // Your code here -- add private members as necessary.
-
     ByteStream _output;  //!< The reassembled in-order byte stream
-    size_t _capacity;    //!< The maximum number of bytes
+    std::set<typeUnassembled> _Unassembled;
+    size_t _firstUnassembled;
+    size_t _nUnassembled;
+    size_t _capacity;  //!< The maximum number of bytes
+    bool _eof;
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
@@ -33,6 +47,9 @@ class StreamReassembler {
     //! \param eof whether or not this segment ends with the end of the stream
     void push_substring(const std::string &data, const uint64_t index, const bool eof);
 
+    // 合并两个_Unassembled的子串，利用iter2更新index和data信息，并删除iter2
+    int merge_substring(size_t &index, std::string &data, std::set<typeUnassembled>::iterator iter2);
+
     //! \name Access the reassembled byte stream
     //!@{
     const ByteStream &stream_out() const { return _output; }
@@ -44,6 +61,11 @@ class StreamReassembler {
     //! \note If the byte at a particular index has been submitted twice, it
     //! should only be counted once for the purpose of this function.
     size_t unassembled_bytes() const;
+
+    size_t first_unassembled_byte() const { return _firstUnassembled; }
+
+    bool eof() const { return _eof; }
+
 
     //! \brief Is the internal state empty (other than the output stream)?
     //! \returns `true` if no substrings are waiting to be assembled
