dae9ae3233d492264f844c32f95d3956cf9c74b7
Yang6149 <614989768@qq.com>
2020-06-01 11:44:06 +0800

    pass lab2

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -7,14 +7,71 @@
 
 template <typename... Targs>
 void DUMMY_CODE(Targs &&... /* unused */) {}
+// //! Our data structure for re-assembling bytes.
+//     StreamReassembler _reassembler;
 
+//     //! The maximum number of bytes we'll store.
+//     size_t _capacity;
+//     bool _syn_received;
+//     WrappingInt32 _isn;
+//     WrappingInt32 _ackno;
 using namespace std;
 
 bool TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
-    return {};
+    bool received = 0;
+    const TCPHeader &hdr = seg.header();
+    // second syn or fin will be rejected
+    //判断是否是合法syn
+    if (hdr.syn == _syn_received)
+        return false;
+    //首次SYN，数据初始化
+    if (hdr.syn) {
+        _isn = hdr.seqno;
+        _syn_received = true;
+        _ackno = _isn;
+        received = 1;
+    }
+    //停止接收数据并且接收到fin
+    if (_reassembler.eof() && hdr.fin)
+        return false;
+    uint64_t win_start = 0;
+    //肯定会进IF
+    if (ackno()) {
+        //val = ackno-isn
+        win_start = unwrap(*ackno(), _isn, 0);
+    }
+    uint64_t abs_seqno = unwrap(hdr.seqno, _isn, win_start);
+    uint64_t seq_size = seg.length_in_sequence_space();
+    if (!seq_size)
+        seq_size = 1;
+
+    uint64_t win_size = window_size();
+    if (!win_size)
+        win_size = 1;
+
+    if (hdr.fin) {
+        //_ackno = _ackno+1;
+        received = 1;
+    }
+    if (hdr.syn)
+        _ackno = _ackno+1;
+
+    if (abs_seqno + seq_size - 1 < win_start)
+        return received;
+    if (win_start + win_size - 1 < abs_seqno)
+        return received;
+    _reassembler.push_substring(seg.payload().copy(), abs_seqno - 1, hdr.fin);  //忽视syn，所以减1
+    _ackno = wrap(_reassembler.first_unassembled_byte(), _isn) + 1;             //+1因为bytestream不给syn标号
+    if (hdr.fin)
+        _ackno = _ackno+1;
+    return true;
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    if (!_syn_received)
+        return nullopt;
+    else
+        return {_ackno};
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const { return stream_out().remaining_capacity(); }
--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -14,8 +14,8 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint32_t N = n & 0xffffffff;
+    return WrappingInt32{isn + N};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +29,9 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint32_t offset = n - wrap(checkpoint, isn);
+    uint64_t ret = checkpoint + offset;
+    if (offset >= (1u << 31) && ret>=(1ul<<32))
+        ret -= (1ul << 32);
+    return ret;
 }
